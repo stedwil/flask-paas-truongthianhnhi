d2daa2c978c5d5082bf274ede3323c10a85ced78
stedwil <nhitruong2018@gmail.com>
2026-09-06 01:14:57 +0700

    feat: them API counter va API info

--- app.py
+++ app.py
@@ -1,35 +1,59 @@
 import datetime
+import os  # Đặt dòng này ở đầu file
 import platform
 from flask import Flask
 
 app = Flask(__name__)
 
+visit_count = 0  # Biến đếm lưu trong RAM
+
 
 @app.route("/")
 def home():
   return f"""
-<html><head><meta charset="utf-8"><title>Flask PaaS Demo</title>
-<style>
-body {{ font-family: Arial; max-width: 640px; margin: 60px auto; }}
-.box {{ background:#DEEAF1; border-left: 5px solid #1F4E79; padding: 24px; border-radius: 8px; }}
-h1 {{ color: #1F4E79; }}
-</style></head><body>
-<h1>Ung dung Flask tren PaaS - phien ban 2!</h1>
-<div class="box">
-<p><b>Sinh vien:</b> Nhi - 233404050198</p>
-<p><b>Mon hoc:</b> Dien toan Dam may </p>
-<p><b>Mo hinh:</b> PaaS - Platform as a Service</p>
-<p><b>Python:</b> {platform.python_version()}</p>
-<p><b>Thoi gian server:</b> {datetime.datetime.now()}</p>
-</div>
-<p>Developer chi viet code - PaaS lo build, deploy, HTTPS, scaling!</p>
-</body></html>
-"""
+    <html><head><meta charset="utf-8"><title>Flask PaaS Demo</title>
+    <style>
+    body {{ font-family: Arial; max-width: 640px; margin: 60px auto; }}
+    .box {{ background:#DEEAF1; border-left: 5px solid #1F4E79; padding: 24px; border-radius: 8px; }}
+    h1 {{ color: #1F4E79; }}
+    </style></head><body>
+    <h1>Ung dung Flask tren PaaS - phien ban 2!</h1>
+    <div class="box">
+    <p><b>Sinh vien:</b> Nhi - 233404050198</p>
+    <p><b>Mon hoc:</b> Dien toan Dam may </p>
+    <p><b>Mo hinh:</b> PaaS - Platform as a Service</p>
+    <p><b>Python:</b> {platform.python_version()}</p>
+    <p><b>Thoi gian server:</b> {datetime.datetime.now()}</p>
+    </div>
+    <p>Developer chi viet code - PaaS lo build, deploy, HTTPS, scaling!</p>
+    </body></html>
+    """
 
 
 @app.route("/health")
 def health():
   return {"status": "ok"}
+
+
+@app.route("/api/counter")
+def counter():
+  global visit_count
+  visit_count += 1
+  return {
+      "so_lan_truy_cap": visit_count,
+      "ghi_chu": "So nay se MAT khi container khoi dong lai!",
+  }
+
+
+@app.route("/api/info")
+def info():
+  ten_sinh_vien = os.environ.get("STUDENT_NAME", "Chua dat bien moi truong")
+  return {
+      "sinh_vien": ten_sinh_vien,
+      "nguon_du_lieu": (
+          "Environment Variable tren Render, KHONG hardcode trong code"
+      ),
+  }
 
 
 if __name__ == "__main__":
