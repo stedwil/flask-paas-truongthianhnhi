d0ca7c506c66f342d8bcd8ed6618169fe34aa026
stedwil <nhitruong2018@gmail.com>
2026-09-06 00:50:24 +0700

    feat: update tieu de v2

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ body {{ font-family: Arial; max-width: 640px; margin: 60px auto; }}
 .box {{ background:#DEEAF1; border-left: 5px solid #1F4E79; padding: 24px; border-radius: 8px; }}
 h1 {{ color: #1F4E79; }}
 </style></head><body>
-<h1>Ung dung Flask tren PaaS</h1>
+<h1>Ung dung Flask tren PaaS - phien ban 2!</h1>
 <div class="box">
 <p><b>Sinh vien:</b> Nhi - 233404050198</p>
 <p><b>Mon hoc:</b> Dien toan Dam may </p>
